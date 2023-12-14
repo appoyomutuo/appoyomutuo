95b020ef9abfa661f3401e8feb1a74c04953b989
appoyomutuo <appoyomutuo@gmail.com>
2023-12-14 02:17:33 +0100

    add blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatMenuModule } from '@angular/material/menu';
@@ -47,11 +49,15 @@ import { AdminNewBlogComponent } from './components/admin-new-blog/admin-new-blo
     AppRoutingModule,
     FormsModule,
     RouterModule.forRoot([]), 
+
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
     MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatMenuModule
--- src/app/components/admin-new-blog/admin-new-blog.component.ts
+++ src/app/components/admin-new-blog/admin-new-blog.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-new-blog.component.css']
 })
 export class AdminNewBlogComponent {
+  formData = {
+    title: '',
+    shortDescription: '',
+    content: '',
+    imagen: null // Aquí puedes almacenar la imagen seleccionada
+  };
 
+  selectedImages: any[] = [];
+
+  submitForm() {
+    // Aquí puedes agregar la lógica para enviar el formulario, incluyendo las imágenes
+    console.log('Entrada de Blog enviada:', this.formData);
+    console.log('Imágenes seleccionadas:', this.selectedImages);
+  }
+
+  onFileSelected(event) {
+    const files = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.selectedImages.push({ url: e.target.result, file: file });
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  removeImage(index: number) {
+    this.selectedImages.splice(index, 1);
+  }
 }
